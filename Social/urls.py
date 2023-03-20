139683f5aa26c6c5da4947f821e15c77d4164654
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-03-20 15:43:53 -0500

    swagger and redoc description

--- Social/urls.py
+++ Social/urls.py
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Journey of Motherhood",
         default_version='v1',
-        description="API for managing books",
+        description="API for utilizing Journey of Motherhood endpoints",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="aliyutimileyin2340@gmail.com"),
         license=openapi.License(name="BSD License"),
