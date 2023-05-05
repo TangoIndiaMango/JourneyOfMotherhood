38f34923ed781440f002e206b5ac53da4ef70e80
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-05-05 11:32:24 -0500

    fixed

--- posts/serializers.py
+++ posts/serializers.py
@@ -26,7 +26,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'description', 'author', 'user',
+        fields = ('id', 'title', 'description', 'author',
                   'created_at', 'updated_at', 'tags', 'image', 'views', 'topic')
 
 
