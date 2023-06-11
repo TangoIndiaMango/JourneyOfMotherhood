bb1814aff17ebbf85705c35d30226ae32c258ea5
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-06-11 15:12:30 -0500

    fixed reset_password

--- UserProfile/urls.py
+++ UserProfile/urls.py
@@ -14,9 +14,7 @@ urlpatterns = [
          name='edit_user_profile'),
     path('profile/password/', UserPasswordView.as_view(), name='user_password'),
     path('change_password/', ChangePasswordView.as_view(), name="change_password"),
-    # path('forgot_password/', ForgotPasswordView.as_view(), name='forgot_password'),
-    
-    # path('reset_password/<uidb64>/<token>/', ResetPasswordView.as_view(), name='reset_password'),
+
     path('password_reset/', include('django_rest_passwordreset.urls'), name='password_reset'),
     
     path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
@@ -26,10 +24,6 @@ urlpatterns = [
     path('confirm-reset-password/<str:uidb64>/<str:token>/', ConfirmResetPasswordView.as_view(), name='confirm-reset-password'),
     path('topusers/', TopUsersView.as_view(), name='top_users'),
     
-
-
-    # path('reset_password/<uidb64>&<token>/', ResetPasswordView.as_view(), name='reset_password'),
-    # path('reset_password/', ResetPasswordView.as_view(), name='reset_password')
     
 
 ]
