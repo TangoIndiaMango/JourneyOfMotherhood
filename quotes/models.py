bc8c92832f0978d7b6301b448121ea5de251f46e
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-06-25 04:39:46 -0500

    return str value of quotes model

--- quotes/models.py
+++ quotes/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Quote(models.Model):
     author = models.CharField(max_length=255)
     description = models.TextField()
+    
+    def __str__(self):
+        return self.author
